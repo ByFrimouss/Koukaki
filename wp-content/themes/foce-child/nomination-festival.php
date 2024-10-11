084695b0c94648c2a5b73979b7810d498734de5e
Stéphanie Coupama <frimouss.graphweb@gmail.com>
2024-10-11 11:29:47 +0400

    Animation de tous les fleurs

---
+++ wp-content/themes/foce-child/nomination-festival.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template part for displaying the festival nomination section
+ *
+ * @package Fleurs_d\'oranger_&_Chats_errants
+ */
+?>
+
+<section>
+    <div>
+        <h3>Le Lieu</h3>
+        <p><?php echo get_theme_mod('place'); ?></p>
+    </div>
+</section>
